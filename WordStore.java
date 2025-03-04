51f780ae0ae7f03e469a3b40bed5bd9bea0d99e2
Gagan <gghatahora1@gmail.com>
2025-03-04 10:16:09 +0000

    new file:   WordStore.class 	modified:   WordStore.java

---
+++ WordStore.java
@@ -0,0 +1,14 @@
+public class WordStore {
+    
+    public WordStore() {
+        
+    }
+
+    public void add(char key, String item) {
+        
+    }
+
+    public String getRandomItem(char key) {
+        return "";
+    }
+}
